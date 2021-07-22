1cd65bd9dd517f43c1eb1d037199b8088320359a
DitoCahyaPratama <ditocahyapratama717@gmail.com>
2021-07-22 17:55:04 +0700

    first commit

---
+++ e2e/Liking_Restaurant.spec.js
@@ -0,0 +1,76 @@
+const assert = require('assert');
+
+Feature('Liking Restaurants');
+
+Before(({ I }) => {
+  I.amOnPage('/#/favorite');
+});
+
+Scenario('showing empty liked restaurants', ({ I }) => {
+  I.seeElement('#query');
+  I.see('No favorite restaurant!', '.post-item__not__found');
+});
+
+Scenario('liking one restaurant and remove it', async ({ I }) => {
+  I.see('No favorite restaurant!', '.post-item__not__found');
+
+  I.amOnPage('/');
+
+  I.seeElement(('.post-item__title'));
+
+  const firstRestaurant = locate('.post-item__title').first();
+  const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.post-item');
+  const likedRestaurantTitle = await I.grabTextFrom('.post-item__title');
+
+  assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
+
+  I.seeElement('.post-item__title');
+  I.click('.post-item__title');
+  I.waitForElement('.category__item', 2); // wait for 2 second
+  I.seeElement('.category__item');
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+  I.amOnPage('/#/favorite');
+});
+
+Scenario('searching restaurants', async ({ I }) => {
+  I.see('No favorite restaurant!', '.post-item__not__found');
+
+  I.amOnPage('/');
+
+  I.seeElement('.post-item__title');
+
+  const titles = [];
+
+  for (let i = 1; i <= 3; i++) {
+    I.click(locate('.post-item__title').at(i));
+    I.seeElement('#likeButton');
+    I.click('#likeButton');
+    titles.push(await I.grabTextFrom('.headline__title-detail'));
+    I.amOnPage('/');
+  }
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('#query');
+
+  const searchQuery = titles[1].substring(1, 3);
+  const matchingRestaurants = titles.filter((title) => title.indexOf(searchQuery) !== -1);
+
+  I.fillField('#query', searchQuery);
+  I.pressKey('Enter');
+
+  const visibleLikedRestaurants = await I.grabNumberOfVisibleElements('.post-item');
+  assert.strictEqual(matchingRestaurants.length, visibleLikedRestaurants);
+
+  matchingRestaurants.forEach(async (title, index) => {
+    const visibleTitle = await (await I.grabTextFrom(locate('.post-item__title').at(index + 1))).toUpperCase();
+    assert.strictEqual(title, visibleTitle);
+  });
+});
